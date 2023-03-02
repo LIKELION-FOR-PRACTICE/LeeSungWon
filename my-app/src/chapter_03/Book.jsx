d54d05e341003ac9f8b81b6055a0ce2b69077111
lsw02 <sw020726@naver.com>
2023-03-02 22:45:55 +0900

    react

--- my-app/src/chapter_03/Book.jsx
+++ my-app/src/chapter_03/Book.jsx
@@ -3,9 +3,8 @@ import React from "react";
 function Book(props) {
     return (
         <div>
-            <h1>{'책 이름은 ${props.name} '}</h1>
-            <h2>{'이 책은 총 ${props.numOfPage} '}
-            <h3>성원아 공부하자</h3></h2>
+            <h1>{`책 이름은 ${props.name} `}</h1>
+            <h2>{`이 책은 총 ${props.numOfPage} `}</h2>
         </div>
     );
 }
