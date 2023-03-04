75ccae74845d9b37a2bfa5c3ec6a6fbc6ac8de87
lsw02 <sw020726@naver.com>
2023-03-05 00:25:09 +0900

    rr

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,12 +10,15 @@ import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <SignUp />
   </React.StrictMode>
 );
 
